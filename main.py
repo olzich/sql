f223a78e247b71e04735b16fab75b86f3c7756d2
olzich <olzich2@gmail.com>
2022-10-17 22:00:47 +0300

    change 1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 2000))
 server.listen(4)
+f = 5
 print('hhh')
 HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
 client_socket, address = server.accept()
@@ -10,4 +11,5 @@ data = client_socket.recv(1024).decode('utf-8')
 print(data)
 content = 'OK'.encode('utf-8')
 client_socket.send(HDRS.encode('utf-8') +  content)
+
 
